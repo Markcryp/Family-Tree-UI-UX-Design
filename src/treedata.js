a962dca20b33fe81b133dcfb29879edb9d8dc64c
Markcryp <mark.njau@student.moringaschool.com>
2025-07-31 21:18:25 +0300

    Update treedata.js

--- src/treedata.js
+++ src/treedata.js
@@ -1,1 +1,22 @@
-
+export const familyTree = {
+  name: 'Mark Mungai',
+  relation: 'Patriarch',
+  children: [
+    {
+      name: 'Grace Mungai',
+      relation: 'Daughter',
+      children: [
+        {
+          name: 'Brad Mungai',
+          relation: 'Grandson',
+          children: [],
+        },
+      ],
+    },
+    {
+      name: 'Bruno Mungai',
+      relation: 'Grandson',
+      children: [],
+    },
+  ],
+};
