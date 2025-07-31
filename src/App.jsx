4c8933c09e3a3cb8893ed65c16ebaafb6679d0f0
Markcryp <mark.njau@student.moringaschool.com>
2025-07-31 21:14:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+// App.jsx
+import React from 'react';
+import TreeRenderer from './TreeRenderer';
+import { familyTree } from './treeData';
 
+function App() {
+  return (
+    <div>
+      <h2>Family Tree</h2>
+      <TreeRenderer data={familyTree} />
+    </div>
+  );
+}
+
+export default App;
